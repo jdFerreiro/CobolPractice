fa767a3f93e8d011e5f83b7487c32d246ae16f3a
jferreiro <fb.jdavid@gmail.com>
2023-02-17 07:42:12 -0400

    Agregar archivos de proyecto.

---
+++ HolaMundo.cbl
@@ -0,0 +1,15 @@
+      *Primer programa en Visual Cobol 
+       identification division.
+       program-id. HolaMundo.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+
+           goback.
+
+       end program HolaMundo.
